7d60e48415610058f5009dfd2ed0c8da91ae8b8e
ShoneSingLone <ShoneSingLone@gmail.com>
2017-09-06 15:14:52 +0800

    【add】Database Config files added

---
+++ project/microsite/db/config.js
@@ -0,0 +1,11 @@
+var mysql = {
+    mysql: {
+        host     : '127.0.0.1',
+        user     : 'name',
+        password : 'dongcidaci',
+        database : 'balabala',
+        port     : 3306
+    }
+}
+module.exports = mysql;
+
