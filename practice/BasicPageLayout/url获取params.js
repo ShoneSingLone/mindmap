f5caa4942cbd6af4a00faa515a64953a203401f5
ShoneSingLone <ShoneSingLone@gmail.com>
2017-11-17 23:40:01 +0800

    【update】使用isArray

--- practice/BasicPageLayout/url获取params.js
+++ practice/BasicPageLayout/url获取params.js
@@ -1,14 +1,12 @@
 function getUrlParam(sUrl, sKey) {
+    var ResultObj, returnResult;
 
-    var kvArray = sUrl.split("?")[1].split("#")[0].split("&").map(function (val, index, arr) {
-        return val.split("=");
-    });
-    var returnResult, ResultObj;
+    ResultObj = function (sUrl) {
+        var kvArray = sUrl.split("?")[1].split("#")[0].split("&").map(function (val, index, arr) {
+            return val.split("=");
+        }),
+            kvObj = {};
 
-    ResultObj = function (kvArray) {
-        var kvObj = {},
-            resultObj = {};
-        // 粗加工,有附加信息的kv对象
         for (var i = 0; i < kvArray.length; i++) {
             var currentArray = kvArray[i];
             //初次直接赋值
@@ -17,35 +15,18 @@ function getUrlParam(sUrl, sKey) {
             } else {
                 // 重复视为有同名参数
                 // 处理成数组
-                // 未处理
-                if (!kvObj[currentArray[0]].isArray) {
+                if (!isArray(kvObj[currentArray[0]])) {
+                    // 未处理
                     var stamp = kvObj[currentArray[0]];
-                    kvObj[currentArray[0]] = {};
-                    kvObj[currentArray[0]].isArray = true;
-                    kvObj[currentArray[0]].length = 0;
-                    kvObj[currentArray[0]][kvObj[currentArray[0]].length++] = stamp;
+                    kvObj[currentArray[0]] = [];
+                    kvObj[currentArray[0]].push(stamp);
                 }
-                // 已经处理
-                kvObj[currentArray[0]][kvObj[currentArray[0]].length++] = currentArray[1];
+                kvObj[currentArray[0]].push(currentArray[1]);
             }
         }
-
-        for (var key in kvObj) {
-            var _val = kvObj[key];
-            if (_val.isArray) {
-                var _kvArray = [];
-                for (var i = 0; i < _val.length; i++) {
-                    _kvArray.push(_val[i]);
-                }
-                resultObj[key] = _kvArray;
-            } else {
-                // 返回参数的值
-                resultObj[key] = _val;
-            }
-        }
-        return resultObj;
+        return kvObj;
     }
-    var resultObj = new ResultObj(kvArray);
+
     returnResult = function (kvObj, key) {
         if (sKey) {
             // 如果参数存在
@@ -61,11 +42,13 @@ function getUrlParam(sUrl, sKey) {
         }
     }
 
-    return returnResult(resultObj, sKey);
+    return returnResult(new ResultObj(sUrl), sKey);
 }
 
 var isArray = Function.isArray || function (obj) {
     return typeof obj === "object" && Object.prototype.toString.call(obj) === "[object Array]";
 }
 
+console.log(getUrlParam("http://www.nowcoder.com?key=1&key=2&key=3&test=4#hehe", ""));
 console.log(getUrlParam("http://www.nowcoder.com?key=1&key=2&key=3&test=4#hehe", "test"));
+console.log(getUrlParam("http://www.nowcoder.com?key=1&key=2&key=3&test=4#hehe", "key"));
