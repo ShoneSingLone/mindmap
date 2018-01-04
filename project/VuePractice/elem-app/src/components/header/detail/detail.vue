66806f0c78fc417bce792d277d4185d5bc8e92ac
ShoneSingLone <ShoneSingLone@gmail.com>
2018-01-04 23:14:45 +0800

    【update】弹出层

---
+++ project/VuePractice/elem-app/src/components/header/detail/detail.vue
@@ -0,0 +1,44 @@
+<style scoped lang="scss">
+.detail {
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  //   width: 100%;
+  //   height: 100%;
+  overflow: auto;
+  transition: all 0.5s;
+  backdrop-filter: blur(10px);
+  background: rgba(7, 17, 28, 0.8);
+}
+</style>
+
+<template>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  props: {
+    isShowDetail: {
+      type: Boolean
+    }
+  },
+
+  data() {
+    return { showDetail: false };
+  },
+  computed: {
+    asdfasdfasdf: function() {
+      return this.isClickDetail;
+    }
+  },
+  methods: {
+    toggleDetail: function() {
+      this.isShowDetail = !this.isShowDetail;
+    }
+  }
+};
+</script>
+  
--- project/VuePractice/elem-app/src/components/header/header.vue
+++ project/VuePractice/elem-app/src/components/header/header.vue
@@ -1,86 +1,3 @@
-<template>
-  <div class="header" v-if="seller">
-    <div class="content-wrapper">
-      <div class="avatar">
-        <img width="64" heifht="64" :src="seller.avatar" alt="">
-      </div>
-
-      <div class="content">
-        <div class="title">
-          <span class="brand"></span>
-          <span class="name">{{seller.name}} </span>
-        </div>
-
-        <div class="description">
-          {{seller.description}}/{{seller.deliveryTime}}分钟送达
-        </div>
-
-        <div class="support" v-if="sellerDescription">
-          <span class="icon" :class="supportIconClass"></span>
-          <span class="text">{{sellerDescription}}</span>
-        </div>
-      </div>
-
-      <div class="support-count" v-if="sellerDescription">
-        <span class="count">{{seller.supports.length}}个</span>
-        <i class="icon-keyboard_arrow_right"></i>
-      </div>
-    </div>
-
-    <div class="bulletin-wrapper">
-      <span class="bulletin-title"></span>
-      <span class="bulletin-text">{{seller.bulletin}}</span>
-      <i class="icon-keyboard_arrow_right"></i>
-    </div>
-
-    <div class="background">
-      <img :src="seller.avatar" width="100%" height="100%">
-    </div>
-
-
-  </div>
-</template>
-
-<script>
-//放在Created里如何？
-const SUPPORTS_TYPE = {
-  0: "decrease",
-  1: "discount",
-  2: "special",
-  3: "invoice",
-  4: "guarantee"
-};
-
-export default {
-  name: "v-header",
-  created: function() {
-    console.log(this.seller);
-  },
-  props: {
-    seller: {
-      type: Object
-    }
-  },
-  computed: {
-    sellerDescription: function() {
-      return this.seller && this.seller.supports
-        ? this.seller.supports[0].description
-        : "";
-    },
-    supportIconClass: function() {
-      return this.seller && this.seller.supports
-        ? SUPPORTS_TYPE[this.seller.supports[0].type]
-        : "";
-    }
-  },
-  data() {
-    return {
-      // seller2: undefined
-    };
-  }
-};
-</script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import "../../common/scss/mixin/mixin";
@@ -238,5 +155,255 @@ export default {
     z-index: -1;
     filter: blur(10px);
   }
+
+  .detail {
+    position: fixed;
+    z-index: 100;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    overflow: auto;
+    transition: all 0.5s;
+    backdrop-filter: blur(10px);
+    background: rgba(7, 17, 27, 0.8);
+
+    &.fade-enter,
+    &.fade-leave-to {
+      transform: translateX(-100px);
+      opacity: 0;
+    }
+
+    .detail-wrapper {
+      width: 100%;
+      min-height: 100%;
+
+      .detail-main {
+        margin-top: 64px;
+        padding-bottom: 64px;
+
+        .name {
+          line-height: 16px;
+          text-align: center;
+          font-size: 16px;
+          font-weight: 700;
+        }
+
+        .star-wrapper {
+          margin-top: 18px;
+          padding: 2px 0;
+          text-align: center;
+        }
+
+        .title {
+          display: flex;
+          width: 80%;
+          margin: 28px auto 24px auto;
+
+          .line {
+            flex: 1;
+            position: relative;
+            top: -6px;
+            border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+          }
+
+          .text {
+            padding: 0 12px;
+            font-weight: 700;
+            font-size: 14px;
+          }
+        }
+
+        .supports {
+          width: 80%;
+          margin: 0 auto;
+
+          .support-item {
+            padding: 0 12px;
+            margin-bottom: 12px;
+            font-size: 0;
+
+            &:last-child {
+              margin-bottom: 0;
+            }
+
+            .icon {
+              display: inline-block;
+              width: 16px;
+              height: 16px;
+              vertical-align: top;
+              margin-right: 6px;
+              background-size: 16px 16px;
+              background-repeat: no-repeat;
+
+              &.decrease {
+                @include bg-image("decrease_2");
+              }
+
+              &.discount {
+                @include bg-image("discount_2");
+              }
+
+              &.guarantee {
+                @include bg-image("guarantee_2");
+              }
+
+              &.invoice {
+                @include bg-image("invoice_2");
+              }
+
+              &.special {
+                @include bg-image("special_2");
+              }
+            }
+
+            .text {
+              line-height: 16px;
+              font-size: 12px;
+            }
+          }
+        }
+
+        .bulletin {
+          width: 80%;
+          margin: 0 auto;
+
+          .content {
+            padding: 0 12px;
+            line-height: 24px;
+            font-size: 12px;
+          }
+        }
+      }
+    }
+
+    .detail-close {
+      position: relative;
+      width: 32px;
+      height: 32px;
+      margin: -64px auto 0 auto;
+      clear: both;
+      font-size: 32px;
+    }
+  }
 }
 </style>
+
+
+<template>
+  <div class="header" v-if="seller">
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img width="64" heifht="64" :src="seller.avatar" alt=""> </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}} </span>
+        </div>
+        <div class="description"> {{seller.description}}/{{seller.deliveryTime}}分钟送达 </div>
+        <div class="support" v-if="sellerDescription">
+          <span class="icon" :class="supportIconClass"></span>
+          <span class="text">{{sellerDescription}}</span>
+        </div>
+      </div>
+      <div class="support-count" v-if="sellerDescription">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
+    </div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right" @click="toggleDetail"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%"> </div>
+    <!-- <header-detail :isShowDetail="isShowDetail"> </header-detail> -->
+    <transition name="fade">
+      <div class="detail" v-if="isShowDetail">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <star :size="48" :score="seller.score"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li class="support-item" v-for="(item, index) in seller.supports">
+                <span class="icon" :class="SUPPORTS_TYPE[seller.supports[index].type]"></span>
+                <span class="text">{{seller.supports[index].description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
+          </div>
+        </div>
+        <div class="detail-close" @click="toggleDetail">
+          <i class="icon-close"></i>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import star from "@c/star/star";
+
+export default {
+  name: "v-header",
+  created: function() {
+    console.log(this.seller);
+  },
+  props: {
+    seller: {
+      type: Object
+    }
+  },
+  created() {
+    //放在Created里如何？
+    this.SUPPORTS_TYPE = {
+      0: "decrease",
+      1: "discount",
+      2: "special",
+      3: "invoice",
+      4: "guarantee"
+    };
+  },
+  data() {
+    return {
+      isShowDetail: false
+    };
+  },
+  methods: {
+    toggleDetail: function() {
+      this.isShowDetail = !this.isShowDetail;
+    }
+  },
+  computed: {
+    sellerDescription: function() {
+      return this.seller && this.seller.supports
+        ? this.seller.supports[0].description
+        : "";
+    },
+    supportIconClass: function() {
+      return this.seller && this.seller.supports
+        ? this.SUPPORTS_TYPE[this.seller.supports[0].type]
+        : "";
+    }
+  },
+  components: {
+    star
+  }
+};
+</script>
+
