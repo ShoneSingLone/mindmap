f80ed2b6de46e65c138b2f22cb248ff1bca54092
ShoneSingLone <ShoneSingLone@gmail.com>
2018-01-24 18:11:33 +0800

    【update】add list for microSite

--- project/microsite/routes/index.js
+++ project/microsite/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "ShoneSingLone's Zone" });
+});
+/* GET home page. */
+router.get('/list', function (req, res, next) {
+  let hrefs = [
+    ["webpack", "dive-into-webpack"]
+  ];
+  res.render('list', {
+    title: "ShoneSingLone's Zone",
+    hrefs: JSON.stringify(hrefs)
+  });
 });
 router.get('/screenshot', function (req, res, next) {
   res.render('upload/screenshot', { title: 'url' });
