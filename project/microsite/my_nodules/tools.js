c376d5f1049966d9ff2f7f161fd32cba081b5f2f
ShoneSingLone <ShoneSingLone@gmail.com>
2018-02-18 23:48:25 +0800

    【add】download

--- project/microsite/my_nodules/tools.js
+++ project/microsite/my_nodules/tools.js
@@ -33,17 +33,16 @@ const
   copyFile = function (source, destination, isDelete = false) {
     if (isDelete) {
       return (function () {
-        return new Promise((resolve, reject) => {
-          fs.copyFile(source, destination, (err) => {
-            if (err) reject(err);
-            else resolve(source + ' was copied to' + destination);
+          return new Promise((resolve, reject) => {
+            fs.copyFile(source, destination, (err) => {
+              if (err) reject(err);
+              else resolve(source + ' was copied to' + destination);
+            });
           });
-        });
-      })()
+        })()
         .then((data) => {
           return unlink(source);
         })
-
     } else {
       return new Promise((resolve, reject) => {
         fs.copyFile(source, destination, (err) => {
@@ -62,21 +61,44 @@ const
       });
     });
   },
+  mkdir = function (path) {
+    return new Promise((resolve, reject) => {
+      fs.exists(path, (exists) => {
+        exists ? resolve(path + 'exist') : fs.mkdir(path, (err) => {
+          if (err) reject(err);
+          else resolve(path + 'exist');
+        })
+      });
+    });
+  },
   //删除非空文件目录，如果path是文件而非文件目录会报错
   rmdir = function (path) {
     return new Promise((resolve, reject) => {
       fs.rmdir(path, (err) => {
-        if (err) reject(err);
-        else resolve(path + ' was removed');
+        if (err) {
+          reject(err);
+        } else {
+          resolve(path + ' was removed');
+        }
       });
     });
-  }
+  },
+  exists = function (path) {
+    return new Promise((resolve, reject) => {
+      fs.exists(path, (exists) => {
+        exists ? resolve(path) :
+          reject(false);
+      })
+    });
+  };
 
 module.exports = {
   print,
   readFile,
   copyFile,
   unlink,
+  mkdir,
   rmdir,
-  writeFile
+  writeFile,
+  exists
 }
--- project/microsite/routes/ajax.js
+++ project/microsite/routes/ajax.js
@@ -5,7 +5,13 @@ const ps = require('current-processes');
 const Tools = require('../my_nodules/tools');
 const T4s = require('../my_nodules/t4screenshot');
 const T4db = require('../my_nodules/t4db');
+const path = require('path');
+const multer = require('multer');
+const UPLOAD_PATH = './private/uploads';
 
+const upload = multer({
+    dest: UPLOAD_PATH
+});
 
 /*
 @see https://www.npmjs.com/package/cors
@@ -79,10 +85,120 @@ router.post('/screenshot', function (req, res, next) {
             }
         })
         .catch(e => {
-            res.json({ e });
+            res.json({
+                e
+            });
         });
 });
 
+//多文件上传
+router.post('/upload', upload.single('file'), function (req, res, next) {
+    const file = req.file;
+    const libPath = path.join('my_nodules/db/uploadLib.json');
+    Tools.exists(file.path).then(r => {
+        return Tools.readFile(libPath)
+    }).then(data => {
+        let libArray = [];
+        try {
+            libArray = JSON.parse(data);
+        } catch (error) {
+            console.log("new libArray");
+        }
+        libArray.push({
+            path: String(file.path),
+            fileName: String(file.originalname)
+        });
+        data = JSON.stringify(libArray);
 
+        return Tools.writeFile(libPath, data);
+    }).then(r => {
+        res.json({
+            msg: file.originalname + '上传成功',
+            status: true
+        })
+    }).catch(err => {
+        console.log("err: ", err);
+        res.json({
+            err
+        })
+    });
+})
+
+
+/**
+ * 获取可下载文件列表
+ */
+router.get('/list/:listType', function (req, res, next) {
+    let param = req.query;
+    let listType = req.params.listType;
+    let rData = "",
+        result = {
+            code: '-200',
+            msg: false,
+            rData: rData
+        },
+        libPath = path.join('my_nodules/db/uploadLib.json');
+
+    Tools.readFile(libPath).then(data => {
+        if (data) {
+            res.json({
+                imgList: JSON.parse(data),
+                status: true
+            })
+        } else {
+            throw new Error("data erro");
+        }
+    }).catch(err => {
+        console.log("err: ", err);
+        res.json({
+            err
+        })
+    });
+});
+
+router.post('/canidownload', function (req, res) {
+    var realPath = req.body.row.path;
+
+    Tools.exists(realPath).then(exists => {
+        if (!exists) {
+            res.json({
+                status: false,
+                err: "文件不存在"
+            })
+        } else {
+            res.json({
+                status: true,
+                url: "文件不存在"
+            })
+            console.log("文件存在");
+            // fs.readFile(realPath, "binary", function (err, file) {
+            //     console.log(err ? "err" : "");
+            //     res.write(realPath, "binary");
+            //     res.end();
+            // });
+            res.download(realPath, String(req.query.name));
+            // res.json({
+            //     status: true,
+            //     code: 200
+            // });
+        }
+
+    })
+});
+router.get('/download', function (req, res) {
+    var realPath = req.query.id;
+    Tools.exists(realPath).then(exists => {
+        if (!exists) {
+            res.json({
+                status: false,
+                code: 404,
+                err: "文件不存在"
+            })
+        } else {
+            res.download(realPath, String(req.query.name));
+        }
+
+    })
+});
 
 module.exports = router;
