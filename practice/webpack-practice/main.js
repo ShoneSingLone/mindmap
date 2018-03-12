93d767406ee410433f2c036dee9678e179af11b4
ShoneSingLone <ShoneSingLone@gmail.com>
2018-03-12 16:52:23 +0800

    【webpack】DevServer

--- practice/webpack-practice/main.js
+++ practice/webpack-practice/main.js
@@ -1,5 +1,5 @@
-// 通过 CommonJS 规范导入 CSS 模块
-require('./style.css');
+// 通过 CommonJS 规范导入 css 模块
+require('./main.css');
 // 通过 CommonJS 规范导入 show 函数
 const show = require('./show.js');
 // 执行 show 函数
--- practice/webpack-practice/show.js
+++ practice/webpack-practice/show.js
@@ -1,9 +1,9 @@
 // 操作 DOM 元素，把 content 显示到网页上
 function show(content) {
-    let eDiv = document.createElement('div');
-    eDiv.innerHTML = ('Hello,' + content + ' ' + Date.now());
-    eDiv.setAttribute('class', 'content');
-    window.document.getElementById('app').appendChild(eDiv);
+  let eDiv = document.createElement('div');
+  eDiv.innerHTML = ('Hello,' + content + ' ' + Date.now());
+  eDiv.setAttribute('class', 'content');
+  window.document.getElementById('app').appendChild(eDiv);
 }
 
 // 通过 CommonJS 规范导出 show 函数
