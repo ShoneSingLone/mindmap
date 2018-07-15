214d202d1d0220c91c3eb5d9dd76e148586c6e7c
ShoneSingLone <ShoneSingLone@gmail.com>
2018-07-15 17:13:26 +0800

    Asset Management

--- project/html5/src/index.js
+++ project/html5/src/index.js
@@ -1,9 +1,17 @@
 import lodash from 'lodash';
+import './style.css';
+import MyImage from './blog.jpg';
+
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = lodash.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    let myIcon = new Image();
+    myIcon.src = MyImage;
+    element.appendChild(myIcon);
     return element;
 }
 
