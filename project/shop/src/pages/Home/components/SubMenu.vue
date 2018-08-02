0bd66f0b45c7cac1058a2929acd36891b0084bd5
ShoneSingLone <ShoneSingLone@gmail.com>
2018-08-02 23:25:56 +0800

    update

---
+++ project/shop/src/pages/Home/components/SubMenu.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="sub-menu hide" id="sub-menu">
+    <inner-box/>
+  </div>
+</template>
+
+<script>
+import InnerBox from './InnerBox'
+
+export default {
+  name: 'SubMenu',
+  data () {
+    return {}
+  },
+  components: {
+    'inner-box': InnerBox
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='scss'>
+</style>
