064b7a6c62e115b625be0d0ad8097387ade15c31
ShoneSingLone <ShoneSingLone@gmail.com>
2018-08-08 12:23:13 +0800

    screen2

--- project/shop/src/pages/XXR/components/Screen2.vue
+++ project/shop/src/pages/XXR/components/Screen2.vue
@@ -2,14 +2,16 @@
   <section class="screen-2">
     <div class="heading-wrapper">
       <div class="heading">精彩，尽在掌握</div>
-      <div class="subheading">采用受欢迎的设计，让它更加出色。
-        <br>款式小巧、轻便手感更舒适。绚丽高清的显示屏，整个外观显得格外精致。</div>
-
+      <div class="subheading">
+        <p>采用受欢迎的设计，让它更加出色。</p>
+        <p>款式小巧、轻便手感更舒适。绚丽高清的显示屏，整个外观显得格外精致。</p>
+      </div>
       <div class="phone">
         <div class="point point_i_1">高清摄像</div>
         <div class="point point_i_2">超薄机身</div>
         <div class="point point_i_3">大屏显示</div>
       </div>
+    </div>
   </section>
 </template>
 
@@ -33,48 +35,46 @@ export default {
 
 .screen-2 {
   // outline: 1px dashed rebeccapurple;
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: flex-start;
-  align-items: center;
-  color: rgb(0, 0, 0);
+  color: rgb(7, 17, 27);
+  height: 800px;
+  background-color: #fafafa;
   text-align: center;
-  // background-color: rgb(129, 179, 228);
-  // background: linear-gradient( 145deg, rgb(129, 179, 228) 0%, #85bbe9 25%, #8ec2ee 50%, #88bbe9 75%, white );
-  background: url(../img/bg-screen-2.jpg) center center/cover no-repeat;
 
-  position: relative;
-  overflow: hidden;
-
-  .wrapper {
+  .heading-wrapper {
     // outline: 1px solid rgb(51, 150, 153);
-    position: relative;
-    @include width();
+    max-width: 1200px;
+    min-width: 800px;
+    margin: 0px auto;
     height: 100%;
+    position: relative;
+    overflow: hidden;
 
     .heading {
       // outline: 2px solid rgb(153, 51, 110);
-      margin-top: 5rem;
+      position: absolute;
+      top: 80px;
       width: 100%;
       font-size: 42px;
-      color: #000;
+      color: #f01414;
     }
     .subheading {
       // outline: 3px dotted rgb(51, 153, 122);
       width: 100%;
-      margin-top: 2rem;
-      font-size: 1rem;
+      top: 190px;
+      position: absolute;
+      font-size: 16px;
       p {
         margin: 0.5rem;
       }
     }
     .phone {
-      display: none;
-      outline: 4px solid rgb(176, 116, 236);
-
-      position: relative;
-      width: 100%;
+      width: 800px;
       height: 873px;
+      background: url(../img/bg-screen-2.png) no-repeat;
+      position: absolute;
+      left: 50%;
+      margin-left: -464px;
+      top: 270px;
       .point {
         font-size: 1.5rem;
         color: #4d555d;
--- project/shop/src/pages/XXR/index.vue
+++ project/shop/src/pages/XXR/index.vue
@@ -11,8 +11,8 @@
 
     <screen1/>
     <screen2/>
-    <!-- 
     <screen3/>
+    <!-- 
     <screen4/>
     <screen5/> 
 
