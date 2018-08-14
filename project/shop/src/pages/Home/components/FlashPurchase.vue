2c30e8e253615b651dc5538ae7e8f67481649a80
ShoneSingLone <ShoneSingLone@gmail.com>
2018-08-14 19:52:30 +0800

    update

---
+++ project/shop/src/pages/Home/components/FlashPurchase.vue
@@ -0,0 +1,242 @@
+<template>
+  <div class="flashPurchase-warpper container">
+
+    <ul class="flashPurchase-countdown">
+      <li class="rainbow-item flashPurchaseInfo">
+        <div class="time-title alshRound">18:00 场</div>
+        <img src="//i1.mifile.cn/f/i/2018/mihome/flashpurchase.png">
+        <div class="sub J_falshDesc">距离结束还有</div>
+        <div class="countdown">
+          <div class="box">00</div>
+          <div class="dosh">:</div>
+          <div class="box">15</div>
+          <div class="dosh">:</div>
+          <div class="box">34</div>
+        </div>
+      </li>
+    </ul>
+
+    <div class="carousel-wrapper">
+
+      <ul class="flashPurchase-list carousel-col-5-list J_purchase_temp J_carouselList" style="width: 2232px; margin-left: -248px; transition: margin-left 0.5s ease 0s;">
+        <li class="item rainbow-item-1">
+          <a href="//www.mi.com/seckill" target="_blank" data-log_code="31activity_homeseckill#goodsid=2174700007&amp;start_time=1534240800&amp;end_time=1534242420&amp;bid=3184494" data-stat-id="1dabd6dc2fdc9775" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-1dabd6dc2fdc9775', '//www.mi.com/seckill', 'pcpid', '31activity_homeseckill#goodsid=2174700007&amp;start_time=1534240800&amp;end_time=1534242420&amp;bid=3184494']);">
+            <div class="bg"></div>
+          </a>
+          <div class="content">
+            <a class="thumb exposure" data-stat-id="b04e578015bd2648" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-b04e578015bd2648', '', 'pcpid', '']);"> <img src="https://i8.mifile.cn/b2c-mimall-media/888410217eac616bea0ad249e6435139.jpg" alt=""> </a>
+            <h3 class="title">
+              <a data-stat-id="c0ea10cfc2a8d857" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-c0ea10cfc2a8d857', '', 'pcpid', '']);">米家蓝牙温湿度计 白色</a>
+            </h3>
+            <p class="desc">冷暖干湿，一目了然</p>
+            <p class="price">
+              <span>1</span>&nbsp;
+              <span>元</span>&nbsp;
+              <del>69元</del>
+            </p>
+          </div>
+        </li>
+        <li class="item rainbow-item-2">
+          <a href="//www.mi.com/seckill" target="_blank" data-log_code="31activity_homeseckill#goodsid=2181700051&amp;start_time=1534240800&amp;end_time=1534242420&amp;bid=3184494" data-stat-id="a5f8622822277d4b" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-a5f8622822277d4b', '//www.mi.com/seckill', 'pcpid', '31activity_homeseckill#goodsid=2181700051&amp;start_time=1534240800&amp;end_time=1534242420&amp;bid=3184494']);">
+            <div class="bg"></div>
+          </a>
+          <div class="content">
+            <a class="thumb exposure" data-stat-id="c328bf52c7086d98" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-c328bf52c7086d98', '', 'pcpid', '']);"> <img src="https://i8.mifile.cn/b2c-mimall-media/f9215dc9023295de56cfc12179672a26.jpg" alt=""> </a>
+            <h3 class="title">
+              <a data-stat-id="429210d04ead69d3" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-429210d04ead69d3', '', 'pcpid', '']);">时怡什锦果仁(罐装1020g)</a>
+            </h3>
+            <p class="desc">5种果仁，用心甄选</p>
+            <p class="price">
+              <span>109</span>&nbsp;
+              <span>元</span>&nbsp;
+              <del>148元</del>
+            </p>
+          </div>
+        </li>
+        <li class="item rainbow-item-3">
+          <a href="//www.mi.com/seckill" target="_blank" data-log_code="31activity_homeseckill#goodsid=2164000003&amp;start_time=1534240800&amp;end_time=1534242420&amp;bid=3184494" data-stat-id="2ca2c532e85f0f1a" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-2ca2c532e85f0f1a', '//www.mi.com/seckill', 'pcpid', '31activity_homeseckill#goodsid=2164000003&amp;start_time=1534240800&amp;end_time=1534242420&amp;bid=3184494']);">
+            <div class="bg"></div>
+          </a>
+          <div class="content">
+            <a class="thumb exposure" data-stat-id="1ce3c9b741349ed1" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-1ce3c9b741349ed1', '', 'pcpid', '']);"> <img src="https://i8.mifile.cn/v1/a1/4544f541-f2e1-f423-5cbf-88fe33c5e110.jpg" alt=""> </a>
+            <h3 class="title">
+              <a data-stat-id="0a94051bd2d3f00a" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-0a94051bd2d3f00a', '', 'pcpid', '']);">空气净化器滤芯 除甲醛增强版</a>
+            </h3>
+            <p class="desc">阻挡细菌，有效去除甲醛</p>
+            <p class="price">
+              <span>139</span>&nbsp;
+              <span>元</span>&nbsp;
+              <del>169元</del>
+            </p>
+          </div>
+        </li>
+        <li class="item rainbow-item-4">
+          <a href="//www.mi.com/seckill" target="_blank" data-log_code="31activity_homeseckill#goodsid=2170600015&amp;start_time=1534240800&amp;end_time=1534242420&amp;bid=3184494" data-stat-id="80becc3939fe0731" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-80becc3939fe0731', '//www.mi.com/seckill', 'pcpid', '31activity_homeseckill#goodsid=2170600015&amp;start_time=1534240800&amp;end_time=1534242420&amp;bid=3184494']);">
+            <div class="bg"></div>
+          </a>
+          <div class="content">
+            <a class="thumb exposure" data-stat-id="4cfa7892df79c4ab" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-4cfa7892df79c4ab', '', 'pcpid', '']);"> <img src="https://i8.mifile.cn/v1/a1/db79032f-f017-5da6-8068-23118a0cf491.jpg" alt=""> </a>
+            <h3 class="title">
+              <a data-stat-id="05779bea7fa6bb2b" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-05779bea7fa6bb2b', '', 'pcpid', '']);">米家空气净化器滤芯 抗菌版 紫色</a>
+            </h3>
+            <p class="desc">净化室内空气看不见的细菌</p>
+            <p class="price">
+              <span>155</span>&nbsp;
+              <span>元</span>&nbsp;
+              <del>159元</del>
+            </p>
+          </div>
+        </li>
+        <li class="item rainbow-item-5">
+          <a href="//www.mi.com/seckill" target="_blank" data-log_code="31activity_homeseckill#goodsid=1183200019&amp;start_time=1534240800&amp;end_time=1534242420&amp;bid=3184494" data-stat-id="ca13ba3620973707" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-ca13ba3620973707', '//www.mi.com/seckill', 'pcpid', '31activity_homeseckill#goodsid=1183200019&amp;start_time=1534240800&amp;end_time=1534242420&amp;bid=3184494']);">
+            <div class="bg"></div>
+          </a>
+          <div class="content">
+            <a class="thumb exposure" data-stat-id="48c4f5f4d7078e1d" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-48c4f5f4d7078e1d', '', 'pcpid', '']);"> <img src="https://i8.mifile.cn/b2c-mimall-media/be87a8db5206f7504720327bba1a24fa.jpg" alt=""> </a>
+            <h3 class="title">
+              <a data-stat-id="267affa25986b180" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-267affa25986b180', '', 'pcpid', '']);">最生活毛巾816感恩套装</a>
+            </h3>
+            <p class="desc">816感恩套装（浴巾+毛巾+方巾） 限量秒杀</p>
+            <p class="price">
+              <span>81.6</span>&nbsp;
+              <span>元</span>&nbsp;
+              <del>133.8元</del>
+            </p>
+          </div>
+        </li>
+        <li class="item rainbow-item-6">
+          <a href="//www.mi.com/seckill" target="_blank" data-log_code="31activity_homeseckill#goodsid=2164800012&amp;start_time=1534240800&amp;end_time=1534242420&amp;bid=3184494" data-stat-id="f54b8994d6f821e1" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-f54b8994d6f821e1', '//www.mi.com/seckill', 'pcpid', '31activity_homeseckill#goodsid=2164800012&amp;start_time=1534240800&amp;end_time=1534242420&amp;bid=3184494']);">
+            <div class="bg"></div>
+          </a>
+          <div class="content">
+            <a class="thumb exposure" data-stat-id="8599267d2d32aa2c" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-8599267d2d32aa2c', '', 'pcpid', '']);"> <img src="https://i8.mifile.cn/v1/a1/0c22de9a-46c2-2c7d-9888-6058ce73d378.jpg" alt=""> </a>
+            <h3 class="title">
+              <a data-stat-id="065f07b304e7bb71" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-065f07b304e7bb71', '', 'pcpid', '']);">米家多功能网关 白色</a>
+            </h3>
+            <p class="desc">米家智能配件控制中心</p>
+            <p class="price">
+              <span>119</span>&nbsp;
+              <span>元</span>&nbsp;
+              <del>149元</del>
+            </p>
+          </div>
+        </li>
+        <li class="item rainbow-item-7">
+          <a href="//www.mi.com/seckill" target="_blank" data-log_code="31activity_homeseckill#goodsid=2174900023&amp;start_time=1534240800&amp;end_time=1534242420&amp;bid=3184494" data-stat-id="1196ab8aabc79cfd" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-1196ab8aabc79cfd', '//www.mi.com/seckill', 'pcpid', '31activity_homeseckill#goodsid=2174900023&amp;start_time=1534240800&amp;end_time=1534242420&amp;bid=3184494']);">
+            <div class="bg"></div>
+          </a>
+          <div class="content">
+            <a class="thumb exposure" data-stat-id="acbc2c5e84143ae8" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-acbc2c5e84143ae8', '', 'pcpid', '']);"> <img src="https://i8.mifile.cn/a1/pms_1512725431.07341927.jpg" alt=""> </a>
+            <h3 class="title">
+              <a data-stat-id="dedb085a108c0b76" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-dedb085a108c0b76', '', 'pcpid', '']);">小米降噪耳机 黑色</a>
+            </h3>
+            <p class="desc">双动圈+动铁声学架构</p>
+            <p class="price">
+              <span>259</span>&nbsp;
+              <span>元</span>&nbsp;
+              <del>299元</del>
+            </p>
+          </div>
+        </li>
+        <li class="item rainbow-item-8">
+          <a href="//www.mi.com/seckill" target="_blank" data-log_code="31activity_homeseckill#goodsid=2163500007&amp;start_time=1534240800&amp;end_time=1534242420&amp;bid=3184494" data-stat-id="7ba6349457f0c64e" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-7ba6349457f0c64e', '//www.mi.com/seckill', 'pcpid', '31activity_homeseckill#goodsid=2163500007&amp;start_time=1534240800&amp;end_time=1534242420&amp;bid=3184494']);">
+            <div class="bg"></div>
+          </a>
+          <div class="content">
+            <a class="thumb exposure" data-stat-id="256bfe48172c2c48" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-256bfe48172c2c48', '', 'pcpid', '']);"> <img src="https://i8.mifile.cn/v1/a1/26f35fd6-ad98-8c3b-8d38-84c797ac7dd2.jpg" alt=""> </a>
+            <h3 class="title">
+              <a data-stat-id="2bf2ad04a470990c" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-2bf2ad04a470990c', '', 'pcpid', '']);">8H护颈乳胶枕</a>
+            </h3>
+            <p class="desc">多重新科技 升级好睡眠</p>
+            <p class="price">
+              <span>229</span>&nbsp;
+              <span>元</span>&nbsp;
+              <del>239元</del>
+            </p>
+          </div>
+        </li>
+        <li class="item rainbow-item-9">
+          <a href="//www.mi.com/seckill" target="_blank" data-log_code="31activity_homeseckill#goodsid=2164100003&amp;start_time=1534240800&amp;end_time=1534242420&amp;bid=3184494" data-stat-id="90e02a349986834c" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-90e02a349986834c', '//www.mi.com/seckill', 'pcpid', '31activity_homeseckill#goodsid=2164100003&amp;start_time=1534240800&amp;end_time=1534242420&amp;bid=3184494']);">
+            <div class="bg"></div>
+          </a>
+          <div class="content">
+            <a class="thumb exposure" data-stat-id="f092492b25c0b11b" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-f092492b25c0b11b', '', 'pcpid', '']);"> <img src="https://i8.mifile.cn/v1/a1/b45d9673-32fa-251a-68b5-791f3cb7e65e.jpg" alt=""> </a>
+            <h3 class="title">
+              <a data-stat-id="619db0c02eb02547" onclick="_msq.push(['trackEvent', '81190ccc4d52f577-619db0c02eb02547', '', 'pcpid', '']);">米兔积木机器人</a>
+            </h3>
+            <p class="desc">978个零件，手机智能遥控</p>
+            <p class="price">
+              <span>399</span>&nbsp;
+              <span>元</span>&nbsp;
+              <del>499元</del>
+            </p>
+          </div>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Carousel',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='scss' scoped>
+@import "../base";
+@import "../../../assets/bootstrap/variables";
+
+.container {
+  margin: 5rem auto;
+  @include width();
+  height: 20rem;
+  display: flex;
+  flex-flow: row nowrap;
+  @include box-shadow();
+
+  ul.flashPurchase-countdown {
+    // outline: 1rem solid rebeccapurple;
+    width: 15rem;
+    height: 100%;
+    background-color: rgb(241, 237, 237);
+
+    .rainbow-item {
+      outline: 1px solid rebeccapurple;
+      display: flex;
+      flex-flow: column nowrap;
+      justify-content: flex-start;
+      align-items: center;
+      .time-title {
+        margin-top: 5rem;
+      }
+      .countdown {
+        display: flex;
+        justify-content: center;
+      }
+    }
+  }
+
+  div.carousel-wrapper {
+    // outline: 1rem solid red;
+    height: 100%;
+    overflow: auto;
+    flex: 1;
+
+    ul {
+      transform: translateX(0);
+      display: flex;
+      flex-flow: row nowrap;
+      li {
+        display: inline-block;
+      }
+    }
+  }
+}
+</style>
--- project/shop/src/pages/Home/index.vue
+++ project/shop/src/pages/Home/index.vue
@@ -18,20 +18,24 @@
       </li>
     </ul>
     <!-- carousel -->
-    <div class="banner" id="banner" v-on:mouseover="carousel.mouseover" v-on:mouseout="carousel.mouseout">
-      <a href="javascript:void(0);" v-for="(slideItem, index) in carousel.slides" :key="index" @click="goTo">
-        <div :class="['slide',slideItem.imgClass, (carousel.currentSlide===index?'active':'')]"></div>
-      </a>
+    <div class="carousel">
+      <div class="banner" id="banner" v-on:mouseover="carousel.mouseover" v-on:mouseout="carousel.mouseout">
+        <a href="javascript:void(0);" v-for="(slideItem, index) in carousel.slides" :key="index" @click="goTo">
+          <div :class="['slide',slideItem.imgClass, (carousel.currentSlide===index?'active':'')]"></div>
+        </a>
+      </div>
+      <a href="javascript:void(0)" class="button prev" id="prev" @click="carousel.clickPrev"></a>
+      <a href="javascript:void(0)" class="button next" id="next" @click="carousel.clickNext"></a>
+      <div class="dots" id="dots">
+        <span :class="{active:carousel.currentSlide === index}" v-for="(dotItem, index) in carousel.slides" :key="index" @click="carousel.clickDots(index)"></span>
+      </div>
     </div>
-    <a href="javascript:void(0)" class="button prev" id="prev" @click="carousel.clickPrev"></a>
-    <a href="javascript:void(0)" class="button next" id="next" @click="carousel.clickNext"></a>
-    <div class="dots" id="dots">
-      <span :class="{active:carousel.currentSlide === index}" v-for="(dotItem, index) in carousel.slides" :key="index" @click="carousel.clickDots(index)"></span>
-    </div>
+    <flash-purchase/>
   </div>
 </template>
 
 <script>
+import flashpurchase from './components/FlashPurchase'
 
 export default {
   name: 'index',
@@ -57,7 +61,7 @@ export default {
     }
   },
   computed: {},
-  components: {},
+  components: { 'flash-purchase': flashpurchase },
   data () {
     return {
       isInSubMenu: false,
@@ -166,217 +170,213 @@ export default {
   }
 }
 </script>
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='scss' scoped>
 @import "../../assets/bootstrap/variables";
 
 $height: 440px;
-$listWidth: 244px;
-
-ul.category-list {
-  position: absolute;
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: flex-start;
-  height: $height;
-  width: $listWidth;
-  color: #fff;
-  background: rgba(0, 0, 0, 0.6);
-  z-index: 2;
-
-  li.category-item {
-    &:hover {
-      cursor: pointer;
-      background: #ff6700;
-      color: #fff;
-
-      div.sub-item-wrapper {
-        display: flex;
-        flex-flow: row nowrap;
-        height: $height;
-        position: absolute;
-        left: $listWidth;
-        top: 0;
-        z-index: 2;
-        border: 1px solid #e0e0e0;
-        border-left: 0;
-
-        @include box-shadow;
-
-        ul.item-row {
-          display: flex;
-          flex-flow: column wrap;
-          height: $height;
-          justify-content: flex-start;
-          background-color: #f0f0f0;
-
-          li.item-col {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            margin: 5px 5px;
-            height: 100px;
-            width: 140px;
-            transition: all 1s;
-
-            &:hover {
-              background-color: #fff;
-
-              @include box-shadow;
-
-              z-index: 2;
-            }
-
-            a {
-              color: #000;
-              padding: 0;
-              display: flex;
-              align-items: center;
-
-              img {
-                height: 20px;
-                width: 20px;
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-
-  li.category-item {
-    div.item-wrapper {
-      &:hover {
-        @include box-shadow;
-      }
-
-      position: relative;
-
-      @include text-shadow;
-
-      padding-left: 3rem;
-
-      .iconfont {
-        border-radius: 50%;
-        font-weight: 900;
-        position: absolute;
-        top: 25px;
-        right: 2rem;
-      }
-    }
-
-    div.sub-item-wrapper {
-      display: none;
-    }
-
-    a {
-      position: relative;
-      color: #fff;
-      line-height: 4rem;
-      text-decoration: none;
-
-      &:link,
-      &:visited {
-        color: #fff;
-      }
-    }
-  }
-}
-
-body {
-  font-family: "微软雅黑";
-  color: #14191e;
-}
+$listWidth: 15rem;
 
 .main {
   width: 1200px;
   height: $height;
   margin: 30px auto;
-  position: relative; //   overflow: hidden;
-}
-
-.banner {
-  //   outline: 1px solid #f01414;
-  width: 1200px;
-  height: $height;
-  // overflow: hidden;
   position: relative;
+  //   overflow: hidden;
 
-  .slide {
+  ul.category-list {
     position: absolute;
-    width: 1200px;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: flex-start;
     height: $height;
-    background-repeat: no-repeat;
-    background-size: 100%;
-    float: left;
-    opacity: 0;
-    transition: all 2s 1s;
+    width: $listWidth;
+    color: #fff;
+    background: rgba(0, 0, 0, 0.6);
+    z-index: 2;
 
-    &.active {
-      opacity: 1;
-      transition: all 1s;
-    }
+    li.category-item {
+      &:hover {
+        cursor: pointer;
+        background: #ff6700;
+        color: #fff;
 
-    &.slide1 {
-      background-image: url(./img/bg1.jpg);
-    }
+        div.sub-item-wrapper {
+          display: flex;
+          flex-flow: row nowrap;
+          height: $height;
+          position: absolute;
+          left: $listWidth;
+          top: 0;
+          z-index: 2;
+          border: 1px solid #e0e0e0;
+          border-left: 0;
 
-    &.slide2 {
-      background-image: url(./img/bg2.jpg);
-    }
+          @include box-shadow;
 
-    &.slide0 {
-      background-image: url(./img/bg3.jpg);
+          ul.item-row {
+            display: flex;
+            flex-flow: column wrap;
+            height: $height;
+            justify-content: flex-start;
+            background-color: #f0f0f0;
+
+            li.item-col {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              margin: 5px 5px;
+              height: 100px;
+              width: 140px;
+              transition: all 1s;
+
+              &:hover {
+                background-color: #fff;
+
+                @include box-shadow;
+
+                z-index: 2;
+              }
+
+              a {
+                color: #000;
+                padding: 0;
+                display: flex;
+                align-items: center;
+
+                img {
+                  height: 20px;
+                  width: 20px;
+                }
+              }
+            }
+          }
+        }
+      }
+
+      div.item-wrapper {
+        &:hover {
+          @include box-shadow;
+        }
+
+        position: relative;
+
+        @include text-shadow;
+
+        padding-left: 3rem;
+
+        .iconfont {
+          border-radius: 50%;
+          font-weight: 900;
+          position: absolute;
+          top: 25px;
+          right: 2rem;
+        }
+      }
+
+      div.sub-item-wrapper {
+        display: none;
+      }
+
+      a {
+        position: relative;
+        color: #fff;
+        line-height: 4rem;
+        text-decoration: none;
+
+        &:link,
+        &:visited {
+          color: #fff;
+        }
+      }
     }
   }
-}
 
-.button {
-  position: absolute;
-  top: 50%;
-  left: $listWidth;
-  height: 5rem;
-  width: 40px;
-  margin-top: -40px;
-  background: url(./img/arrow.png) center center no-repeat;
+  .carousel {
+    .banner {
+      //   outline: 1px solid #f01414;
+      width: 1200px;
+      height: $height;
+      // overflow: hidden;
+      position: relative;
 
-  &:hover {
-    background-color: #333;
-    opacity: 0.8;
-    filter: alpha(opacity = 80);
-  }
+      .slide {
+        position: absolute;
+        width: 1200px;
+        height: $height;
+        background-repeat: no-repeat;
+        background-size: 100%;
+        float: left;
+        opacity: 0;
+        transition: all 2s 1s;
 
-  &.prev {
-    transform: rotate(180deg);
-  }
+        &.active {
+          opacity: 1;
+          transition: all 1s;
+        }
 
-  &.next {
-    left: auto;
-    right: 0;
-  }
-}
+        &.slide1 {
+          background-image: url(./img/bg1.jpg);
+        }
 
-.dots {
-  position: absolute;
-  bottom: 1.5rem;
-  right: 0;
-  text-align: right;
-  padding-right: 1.5rem;
-  line-height: 12px;
+        &.slide2 {
+          background-image: url(./img/bg2.jpg);
+        }
 
-  span {
-    display: inline-block;
-    width: 12px;
-    height: 12px;
-    border-radius: 50%;
-    margin-left: 8px;
-    background-color: rgba(7, 17, 27, 0.4);
-    cursor: pointer;
-    box-shadow: 0 0 0 2px rgba(255, 255, 255, 0.8) inset;
+        &.slide0 {
+          background-image: url(./img/bg3.jpg);
+        }
+      }
+    }
+    .button {
+      position: absolute;
+      top: 50%;
+      left: $listWidth;
+      height: 5rem;
+      width: 40px;
+      margin-top: -40px;
+      background: url(./img/arrow.png) center center no-repeat;
 
-    &.active {
-      box-shadow: 0 0 0 2px rgba(7, 17, 27, 0.4) inset;
-      background-color: #ffffff;
+      &:hover {
+        background-color: #333;
+        opacity: 0.8;
+        filter: alpha(opacity = 80);
+      }
+
+      &.prev {
+        transform: rotate(180deg);
+      }
+
+      &.next {
+        left: auto;
+        right: 0;
+      }
+    }
+
+    .dots {
+      position: absolute;
+      bottom: 1.5rem;
+      right: 0;
+      text-align: right;
+      padding-right: 1.5rem;
+      line-height: 12px;
+
+      span {
+        display: inline-block;
+        width: 12px;
+        height: 12px;
+        border-radius: 50%;
+        margin-left: 8px;
+        background-color: rgba(7, 17, 27, 0.4);
+        cursor: pointer;
+        box-shadow: 0 0 0 2px rgba(255, 255, 255, 0.8) inset;
+
+        &.active {
+          box-shadow: 0 0 0 2px rgba(7, 17, 27, 0.4) inset;
+          background-color: #ffffff;
+        }
+      }
     }
   }
 }
